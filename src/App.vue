9889a8dd02f15efcdf694eb18792026477185ab8
Christine <31845754+thechubby@users.noreply.github.com>
2017-12-14 23:00:00 +0500

    Sdelana knopka dobavleniya kontakta v spisok

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,71 @@
 <template>
+<div id="container">
+  <div id="input">
+    <input v-model="input_name" placeholder="Введите имя">
+    <input v-model="input_phone" placeholder="Введите номер">
+    <button type="button" class="btn btn-default" v-on:click="add">Добавить!</button>
+  </div>
+
   <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
     <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
+      <li v-for="contact in contacts">{{contact.name}}: {{contact.phone}}</li>
     </ul>
   </div>
+</div>
+
 </template>
 
 <script>
+
 export default {
   name: 'app',
+  components: {
+  },
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to Your Vue.js App',
+      contacts: [
+        {
+          name: 'Иванов Иван',
+          phone: '+7 999 888 77 66'
+        },
+        {
+          name: 'Гуси Лебеди',
+          phone: '+7 123 456 78 90'
+        },
+        {
+          name: 'Петров Петр',
+          phone: '+7 987 654 32 10'
+        },
+        {
+          name: 'Васильев Василий',
+          phone: '+7 999 888 77 66'
+        }
+      ],
+      input_name: '',
+      input_phone: '',
+    }
+  },
+  methods: {
+    add: function () {
+      this.contacts.push({
+        name: this.input_name,
+        phone: this.input_phone
+      })
     }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+#container {
+  border: 1px solid black;
+  width: 500px;
+  margin: auto;
+}
+#input {
+  margin: 15px 0;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
 </style>
