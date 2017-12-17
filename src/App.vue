b15c2dc63028b08424755355ef3cde2b1a45af13
Christine <31845754+thechubby@users.noreply.github.com>
2017-12-17 19:59:55 +0500

    Sdelana knopka redaktirovaniya kontakta

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
 <div id="container">
-  <div id="input">
+  <div id="input" class="centerText marginVert">
     <input v-model="input_name" placeholder="Введите имя">
     <input v-model="input_phone" placeholder="Введите номер">
     <button type="button" class="btn btn-default" v-on:click="add">Добавить!</button>
@@ -8,14 +8,18 @@
 
   <div id="app">
     <ul>
-      <li v-for="contact in contacts">{{contact.name}}: {{contact.phone}}</li>
+      <li v-for="contact in contacts">{{contact.name}}: {{contact.phone}} <button type="button" class="btn btn-default" v-on:click="edit(contacts.indexOf(contact))">Обновить!</button></li>
     </ul>
+  </div>
+  <div id='description' class="centerText marginVert">
+    <p>Для добавления записи введите данные и нажмите "Добавить!"<br>Для редактирования записи введите данные и нажмите "Обновить!"</p>
   </div>
 </div>
 
 </template>
 
 <script>
+import _ from 'lodash';
 
 export default {
   name: 'app',
@@ -23,9 +27,8 @@ export default {
   },
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App',
       contacts: [
-        {
+        {  
           name: 'Иванов Иван',
           phone: '+7 999 888 77 66'
         },
@@ -41,9 +44,7 @@ export default {
           name: 'Васильев Василий',
           phone: '+7 999 888 77 66'
         }
-      ],
-      input_name: '',
-      input_phone: '',
+      ]
     }
   },
   methods: {
@@ -52,7 +53,18 @@ export default {
         name: this.input_name,
         phone: this.input_phone
       })
+    },
+    edit: function(contact) {
+        this.contacts[contact] = {
+          name: this.input_name,
+          phone: this.input_phone
+      } //Создаем и сразу же удаляем последний эл-т массива для повторного рендеринга v-for
+        this.contacts.push({})
+        this.contacts.pop()
     }
+  },
+  watch: {
+    
   }
 }
 </script>
@@ -63,9 +75,16 @@ export default {
   width: 500px;
   margin: auto;
 }
-#input {
-  margin: 15px 0;
+.centerText {
   text-align: center;
 }
-
+.marginVert {
+  margin: 15px 0px;
+}
+ul {
+  border-bottom: 1px solid black;
+  border-top: 1px solid black;
+  padding-top: 15px;
+  padding-bottom: 15px;
+}
 </style>
